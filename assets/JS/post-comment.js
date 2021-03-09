b04feefd081836be6a8bad0aae4a9ca7d0389971
Amul <amulagrawal32@gmail.com>
2021-03-09 21:38:27 +0530

    Update the comment function
    Signed-off-by: Amul <amulagrawal32@gmail.com>

--- assets/JS/post-comment.js
+++ assets/JS/post-comment.js
@@ -1,19 +1,24 @@
 console.log('Checking for post reply')
 
-var btnComment = document.getElementById('comments')
+// var btnComment = document.getElementById('comments')
 var commentContent = document.getElementById('comment-container');
 
-commentContent.style.display = 'none'
+// commentContent.style.display = 'none'
 
 
-btnComment.addEventListener('click',function()
+$('.comments').click(function()
 {
-    if(commentContent.style.display == 'none')
-    {
-        commentContent.style.display = 'block';
-    }
-    else
-    {
-        commentContent.style.display = 'none'
-    }
+    // if(commentContent.style.display == 'none')
+    // {
+    //     commentContent.style.display = 'block';
+    // }
+    // else
+    // {
+    //     commentContent.style.display = 'none'
+    // }
+    console.log("hiie")
+    $(".comment-container").toggleClass("show");
+  
+
+
 })
